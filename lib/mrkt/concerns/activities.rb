c514d8482fd25e83567d82b6f052a989fb2aed04
Corey Finley <finley.corey@gmail.com>
2015-11-02 13:18:56 -0500

    pulls page following from activities lookup

--- lib/mrkt/concerns/activities.rb
+++ lib/mrkt/concerns/activities.rb
@@ -4,32 +4,9 @@ module Mrkt
       get("/rest/v1/activities/types.json")
     end
 
-    def get_activities(first_paging_token, date, activity_type_ids)
-      activities = []
-      next_page_token = first_paging_token
-      more_data = true
-
-      while more_data
-        params = { nextPageToken: next_page_token, activityTypeIds: activity_type_ids }
-        response = get("/rest/v1/activities.json", params)
-
-        result = response[:result] || []
-
-        if Date.parse(result.last[:activityDate]) > date
-          result.reject! do |activity|
-            Date.parse(activity[:activityDate]) > date
-          end
-          activities.push(*result)
-          break
-        end
-
-        activities.push(*result)
-
-        more_data = response[:moreResult]
-        next_page_token = response[:nextPageToken]
-      end
-
-      activities
+    def get_activities(page_token, date, activity_type_ids)
+      params = { nextPageToken: page_token, activityTypeIds: activity_type_ids }
+      get("/rest/v1/activities.json", params)
     end
   end
 end
--- spec/concerns/activities_spec.rb
+++ spec/concerns/activities_spec.rb
@@ -63,7 +63,7 @@ describe Mrkt::Activities do
       {
         "requestId":"a9ae#148add1e53d",
         "success":true,
-        "nextPageToken":first_response_next_page_token,
+        "nextPageToken":"EYS2MBWKQYDAORQGA5D",
         "moreResult":true,
         "result":[
           {
@@ -88,165 +88,14 @@ describe Mrkt::Activities do
       }
     end
 
-    context "same day second response" do
-      let(:first_response_next_page_token) do
-        "GIYDAOBNGEYS2MBWKQYDAORQGA5DAMBOGAYDAKZQGAYDALBRGA3TQ==="
-      end
-      let(:second_response_stub) do
-        {
-          "requestId":"a9ae#148add1e53e",
-          "success":true,
-          "moreResult":false,
-          "result":[
-            {
-              "id":2,
-              "leadId":7,
-              "activityDate":"2013-09-25T07:56:35+0000",
-              "activityTypeId":12,
-              "primaryAttributeValueId":6,
-              "primaryAttributeValue":"Owyliphys Iledil",
-              "attributes":[ { "name":"Source Type", "value":"Web page visit" } ]
-            },
-            {
-              "id":3,
-              "leadId":8,
-              "activityDate":"2013-09-25T08:56:35+0000",
-              "activityTypeId":1,
-              "primaryAttributeValueId":4,
-              "primaryAttributeValue":"anti-phishing",
-              "attributes":[ { "name":"Query Parameters", "value":nil } ]
-            }
-          ]
-        }
-      end
-      let(:expected_combined_result) do
-        [
-          {
-            "id":2,
-            "leadId":6,
-            "activityDate":"2013-09-25T00:39:45+0000",
-            "activityTypeId":12,
-            "primaryAttributeValueId":6,
-            "primaryAttributeValue":"Owyliphys Iledil",
-            "attributes":[ { "name":"Source Type", "value":"Web page visit" } ]
-          },
-          {
-            "id":3,
-            "leadId":9,
-            "activityDate":"2013-09-25T06:56:35+0000",
-            "activityTypeId":1,
-            "primaryAttributeValueId":4,
-            "primaryAttributeValue":"anti-phishing",
-            "attributes":[ { "name":"Query Parameters", "value":nil } ]
-          },
-          {
-            "id":2,
-            "leadId":7,
-            "activityDate":"2013-09-25T07:56:35+0000",
-            "activityTypeId":12,
-            "primaryAttributeValueId":6,
-            "primaryAttributeValue":"Owyliphys Iledil",
-            "attributes":[ { "name":"Source Type", "value":"Web page visit" } ]
-          },
-          {
-            "id":3,
-            "leadId":8,
-            "activityDate":"2013-09-25T08:56:35+0000",
-            "activityTypeId":1,
-            "primaryAttributeValueId":4,
-            "primaryAttributeValue":"anti-phishing",
-            "attributes":[ { "name":"Query Parameters", "value":nil } ]
-          }
-        ]
-      end
+    subject { client.get_activities(first_page_token, date, activity_type_ids) }
 
-      subject { client.get_activities(first_page_token, date, activity_type_ids) }
-
-      before do
-        activity_type_ids_params = activity_type_ids.map { |id| "activityTypeIds=#{id}" }.join("&")
-        stub_request(:get, "https://#{host}/rest/v1/activities.json?nextPageToken=#{first_page_token}&#{activity_type_ids_params}")
-          .to_return(json_stub(first_response_stub))
-        stub_request(:get, "https://#{host}/rest/v1/activities.json?nextPageToken=#{first_response_next_page_token}&#{activity_type_ids_params}")
-          .to_return(json_stub(second_response_stub))
-      end
-
-      it { is_expected.to eq(expected_combined_result) }
+    before do
+      activity_type_ids_params = activity_type_ids.map { |id| "activityTypeIds=#{id}" }.join("&")
+      stub_request(:get, "https://#{host}/rest/v1/activities.json?nextPageToken=#{first_page_token}&#{activity_type_ids_params}")
+        .to_return(json_stub(first_response_stub))
     end
 
-    context "second response includes data outside of date scope" do
-      let(:first_response_next_page_token) do
-        "GIYDAOBNGEYS2MBWKQYDAORQGA5DAMBOGAYDAKZQGAYDALBRGA3TQ==="
-      end
-      let(:second_response_stub) do
-        {
-          "requestId":"a9ae#148add1e53e",
-          "success":true,
-          "moreResult":false,
-          "result":[
-            {
-              "id":2,
-              "leadId":7,
-              "activityDate":"2013-09-25T07:56:35+0000",
-              "activityTypeId":12,
-              "primaryAttributeValueId":6,
-              "primaryAttributeValue":"Owyliphys Iledil",
-              "attributes":[ { "name":"Source Type", "value":"Web page visit" } ]
-            },
-            {
-              "id":3,
-              "leadId":8,
-              "activityDate":"2013-09-26T00:56:35+0000",
-              "activityTypeId":1,
-              "primaryAttributeValueId":4,
-              "primaryAttributeValue":"anti-phishing",
-              "attributes":[ { "name":"Query Parameters", "value":nil } ]
-            }
-          ]
-        }
-      end
-      let(:expected_combined_result) do
-        [
-          {
-            "id":2,
-            "leadId":6,
-            "activityDate":"2013-09-25T00:39:45+0000",
-            "activityTypeId":12,
-            "primaryAttributeValueId":6,
-            "primaryAttributeValue":"Owyliphys Iledil",
-            "attributes":[ { "name":"Source Type", "value":"Web page visit" } ]
-          },
-          {
-            "id":3,
-            "leadId":9,
-            "activityDate":"2013-09-25T06:56:35+0000",
-            "activityTypeId":1,
-            "primaryAttributeValueId":4,
-            "primaryAttributeValue":"anti-phishing",
-            "attributes":[ { "name":"Query Parameters", "value":nil } ]
-          },
-          {
-            "id":2,
-            "leadId":7,
-            "activityDate":"2013-09-25T07:56:35+0000",
-            "activityTypeId":12,
-            "primaryAttributeValueId":6,
-            "primaryAttributeValue":"Owyliphys Iledil",
-            "attributes":[ { "name":"Source Type", "value":"Web page visit" } ]
-          }
-        ]
-      end
-
-      subject { client.get_activities(first_page_token, date, activity_type_ids) }
-
-      before do
-        activity_type_ids_params = activity_type_ids.map { |id| "activityTypeIds=#{id}" }.join("&")
-        stub_request(:get, "https://#{host}/rest/v1/activities.json?nextPageToken=#{first_page_token}&#{activity_type_ids_params}")
-          .to_return(json_stub(first_response_stub))
-        stub_request(:get, "https://#{host}/rest/v1/activities.json?nextPageToken=#{first_response_next_page_token}&#{activity_type_ids_params}")
-          .to_return(json_stub(second_response_stub))
-      end
-
-      it { is_expected.to eq(expected_combined_result) }
-    end
+    it { is_expected.to eq(first_response_stub) }
   end
 end
